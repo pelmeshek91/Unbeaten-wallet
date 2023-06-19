8ce531ff356d28a6b74387572e0178fcf1ef35de
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2023-06-19 13:39:03 +0100

    fix routes

--- src/components/TransactionsIncome/TransactionsIncome.jsx
+++ src/components/TransactionsIncome/TransactionsIncome.jsx
@@ -1,8 +1,17 @@
+import TotalBalance from 'components/TotalBalance/TotalBalance';
+import { NavLink } from 'react-router-dom';
 
 const TransactionsIncome = () => {
   return (
-    <div>TransactionsIncome</div>
-  )
-}
+    <div>
+      <TotalBalance />
+      <div>
+        <NavLink to="/">EXPENSES</NavLink>
+        <NavLink to="/transactions-income">INCOME</NavLink>
+      </div>
+      <h1>transactions</h1>
+    </div>
+  );
+};
 
-export default TransactionsIncome
+export default TransactionsIncome;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,16 @@
 import TotalBalance from 'components/TotalBalance/TotalBalance';
-// import TransactionsExpenses from 'components/TransactionsExpenses/TransactionsExpenses';
-import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+import TransactionsExpenses from 'components/TransactionsExpenses/TransactionsExpenses';
+import { NavLink } from 'react-router-dom';
 
 const HomePage = () => {
   return (
     <>
       <TotalBalance />
-      <Suspense>
-        <Outlet />
-      </Suspense>
+      <div>
+        <NavLink to="/">EXPENSES</NavLink>
+        <NavLink to="transactions-income">INCOME</NavLink>
+      </div>
+      <TransactionsExpenses />
     </>
   );
 };
