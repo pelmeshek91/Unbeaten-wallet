72b919cb27e236aec7d1aec885f513981f27c6ea
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2023-06-20 20:32:43 +0100

    report

--- src/pages/ReportPage/ReportPage.jsx
+++ src/pages/ReportPage/ReportPage.jsx
@@ -1,5 +1,21 @@
+import { useRef } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
 const ReportPage = () => {
-  return <div>ReportPage</div>;
+  const navigate = useNavigate();
+  const location = useLocation();
+  const backLinkLocationRef = useRef(location?.state?.from ?? '/');
+
+  const onBtnClick = () => {
+    navigate(backLinkLocationRef.current);
+  };
+  return (
+    <div>
+      <button type="button" onClick={onBtnClick}>
+        Main page
+      </button>
+    </div>
+  );
 };
 
 export default ReportPage;
