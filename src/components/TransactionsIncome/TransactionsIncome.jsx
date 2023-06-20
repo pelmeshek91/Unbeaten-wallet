8559c04cb735dd025a37337e81349b2a187c9aa8
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2023-06-20 11:03:33 +0100

    add fun

--- src/components/TransactionsIncome/TransactionsIncome.jsx
+++ src/components/TransactionsIncome/TransactionsIncome.jsx
@@ -1,13 +1,16 @@
 import { TransactionDate } from 'components/Date/Date';
 import { TransactionForm } from 'components/Form/Form';
 import TotalBalance from 'components/TotalBalance/TotalBalance';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { loginUserThunk } from 'redux/auth/authOperations';
 import {
   addTransactionExpensesThunk,
   getTransactionsExpensesThunk,
+  getUserInfoThunk,
+  updateUserBalanceThunk,
 } from 'redux/transcactions/transcactionsOperations';
+
 // import { getTransactionsExpenses } from 'services/walletApi';
 
 // import { loginUser } from 'services/walletApi';
@@ -17,8 +20,8 @@ const TransactionsIncome = () => {
   // const sid = useSelector(state => state.auth.sid);
   // const refresh = useSelector(state => state.auth.refreshToken);
   // const expenses = useSelector(state => state.transactions.expenses);
-  // const balance = useSelector(state => state.transactions.balance);
-  // console.log(balance);
+  const balance = useSelector(state => state.transactions.balance);
+  console.log(balance);
   return (
     <>
       <TransactionForm />
@@ -26,24 +29,34 @@ const TransactionsIncome = () => {
       <div>
         <button onClick={() => dispatch(loginUserThunk())}>LOGIN</button>
         <button
-          onClick={() =>
-            // dispatch(refreshUserThunk({ sid, refresh }))
-            dispatch(getTransactionsExpensesThunk())
+          onClick={
+            () => dispatch(getUserInfoThunk())
+            // dispatch(getTransactionsExpensesThunk())
+            // getUserInfo()
           }
         >
           GET
         </button>
         <button
-          onClick={() =>
-            // dispatch(refreshUserThunk({ sid, refresh }))
-            dispatch(
-              addTransactionExpensesThunk({
-                description: 'Expense description',
-                amount: 100,
-                date: '2020-12-31',
-                category: 'Продукты',
-              })
-            )
+          onClick={
+            () =>
+              // dispatch(refreshUserThunk({ sid, refresh }))
+              // dispatch(
+              //   addTransactionExpensesThunk({
+              //     description: 'Expense description',
+              //     amount: 100,
+              //     date: '2020-12-31',
+              //     category: 'Продукты',
+              //   })
+              // )
+              dispatch(
+                updateUserBalanceThunk({
+                  newBalance: 999,
+                })
+              )
+            // updateUserBalance({
+            //   newBalance: 333,
+            // })
           }
         >
           PUSH
