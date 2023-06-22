e496b72852209fe61aca4044c64393073d1bf90e
maksmvc <124098393+maksmvc@users.noreply.github.com>
2023-06-22 22:49:32 +0300

    commit rep page

--- src/pages/ReportPage/ReportPage.jsx
+++ src/pages/ReportPage/ReportPage.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import ReportsExpenses from 'components/ReportsExpenses/ReportsExpenses';
+import ReportChanger from 'components/ReportChanger/ReportChanger';
 
 const ReportPage = () => {
   const navigate = useNavigate();
@@ -7,9 +8,13 @@ const ReportPage = () => {
   const onBtnClick = () => {
     navigate('/expenses');
   };
-
+  const handleClick = selectedPeriod => {
+    // Логика обработки выбранного периода
+    console.log(selectedPeriod);
+  };
   return (
     <>
+      {/* <ReportChanger onClick={handleClick} /> */}
       <div>
         <button type="button" onClick={onBtnClick}>
           Main page
