81dd3507d1d427d49a152c4b32cd564b71de75e4
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2023-06-23 18:25:32 +0100

    add condition chart

--- src/pages/ReportPage/ReportPage.jsx
+++ src/pages/ReportPage/ReportPage.jsx
@@ -4,6 +4,7 @@ import ReportChanger from 'components/ReportChanger/ReportChanger';
 import ReportAmount from 'components/ReportsIncome/ReportsAmount';
 import { BsArrowLeft } from 'react-icons/bs';
 import { BackBtn, BackContainer, BtnName } from './ReportPage.styled';
+import TotalBalance from 'components/TotalBalance/TotalBalance';
 
 const ReportPage = () => {
   const navigate = useNavigate();
@@ -14,13 +15,17 @@ const ReportPage = () => {
 
   return (
     <>
-      <ReportChanger />
-      <BackContainer>
-        <BackBtn type="button" onClick={onBtnClick}>
-          <BsArrowLeft style={{ width: '24px', height: '24px' }} />
-        </BackBtn>
-        <BtnName>Main page</BtnName>
-      </BackContainer>
+      <>
+        <ReportChanger />
+        <BackContainer>
+          <BackBtn type="button" onClick={onBtnClick}>
+            <BsArrowLeft style={{ width: '24px', height: '24px' }} />
+          </BackBtn>
+          <BtnName>Main page</BtnName>
+        </BackContainer>
+        <TotalBalance />
+      </>
+
       <ReportAmount />
       <ReportsExpenses />
     </>
