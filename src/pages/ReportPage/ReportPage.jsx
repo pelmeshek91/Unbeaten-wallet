2ffd011b13ed2c9e1ea150277861e52dc0c97130
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-06-23 12:58:59 +0200

    styled back btn

--- src/pages/ReportPage/ReportPage.jsx
+++ src/pages/ReportPage/ReportPage.jsx
@@ -2,6 +2,8 @@ import { useNavigate } from 'react-router-dom';
 import ReportsExpenses from 'components/ReportsExpenses/ReportsExpenses';
 import ReportChanger from 'components/ReportChanger/ReportChanger';
 import ReportAmount from 'components/ReportsIncome/ReportsAmount';
+import { BsArrowLeft } from 'react-icons/bs';
+import { BackBtn, BackContainer, BtnName } from './ReportPage.styled';
 
 const ReportPage = () => {
   const navigate = useNavigate();
@@ -15,11 +17,12 @@ const ReportPage = () => {
   return (
     <>
       <ReportChanger onClick={handleClick} />
-      <div>
-        <button type="button" onClick={onBtnClick}>
-          Main page
-        </button>
-      </div>
+      <BackContainer>
+        <BackBtn type="button" onClick={onBtnClick}>
+          <BsArrowLeft style={{ width: '24px', height: '24px' }} />
+        </BackBtn>
+        <BtnName>Main page</BtnName>
+      </BackContainer>
       <ReportAmount />
       <ReportsExpenses />
     </>
