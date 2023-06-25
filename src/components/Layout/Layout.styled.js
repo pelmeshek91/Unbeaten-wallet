fbc6e1dc7a92e3454782cefd62c652a921e3b9c5
Maryna Bonko <bonko23@gmail.com>
2023-06-25 16:27:34 +0300

    “header-modals”

--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -3,9 +3,24 @@ import { createGlobalStyle } from 'styled-components';
 import desktopRectangle from '../../img/decor-img/Rectangle-desctop@2x.png';
 import tabletRectangle from '../../img/decor-img/Rectangle-tablet@2x.png';
 export const Container = styled.main`
-  /* width: 1280px; */
-  max-width: 1280px;
   margin: 0 auto;
+  @media screen and (max-width: 767px) {
+    padding-left: 20px;
+    padding-right: 20px;
+    max-width: 767px;
+  }
+
+  @media screen and (min-width: 768px) {
+    padding-left: 32px;
+    padding-right: 32px;
+    max-width: 1023px;
+  }
+
+  @media screen and (min-width: 1024px) {
+    padding-left: 118px;
+    padding-right: 118px;
+    max-width: 1280px;
+  }
 `;
 
 export const GlobalStyle = createGlobalStyle`
